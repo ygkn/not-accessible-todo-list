b1a75882d5da5fa6fc1c525c20de668f5657bc91
ygkn <ygkn@users.noreply.github.com>
2024-06-03 13:25:55 +0900

    introduce ress.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
-import "./index.css";
+
+import "ress/dist/ress.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
